d9d4bad89a1d7b32e475937a91c42fbc68a1c98e
lyy0709 <2722707908@qq.com>
2024-07-11 16:23:27 +0800

    fix

---
+++ handlers/handlers.go
@@ -0,0 +1,21 @@
+package handlers
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+// LoginPage serves the login page
+func LoginPage(c *fiber.Ctx) error {
+	return c.SendFile("./login.html")
+}
+
+// Login handles the session key submission
+func Login(c *fiber.Ctx) error {
+	sessionKey := c.FormValue("sessionKey")
+	c.Cookie(&fiber.Cookie{
+		Name:  "sessionKey",
+		Value: sessionKey,
+		Path:  "/",
+	})
+	return c.Redirect("/")
+}
--- router/router.go
+++ router/router.go
@@ -2,12 +2,19 @@ package router
 
 import (
 	"fkclaude/serve"
+	"fkclaude/handlers"
 	"github.com/gofiber/fiber/v2"
-
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func SetupRoutes(app *fiber.App) {
 	app.Use(logger.New(logger.ConfigDefault))
+
+	// Serve the login page
+	app.Get("/login", handlers.LoginPage)
+	// Handle session key submission
+	app.Post("/login", handlers.Login)
+
+	// Handle all other requests
 	serve.APIHandler(app)
 }
